abea399b606c8e5516205a225d5165b93793dbfa
Scott M Parrish <anithri@gmail.com>
2017-10-31 00:17:54 -0600

    configure for redux-dev-tools

--- src/Store.js
+++ src/Store.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { compose, createStore, applyMiddleware, combineReducers } from 'redux';
 import { auto, reducers } from 'redux-auto';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 const webpackModules = require.context("./store", true, /^.*\.js$/);
 
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
 class Store extends React.Component {
   state = {};
+
   componentWillMount() {
-    const middleware = applyMiddleware( auto(webpackModules, webpackModules.keys()));
-    const store = createStore(combineReducers(reducers), middleware );
+    const enhancer = composeEnhancers(
+      applyMiddleware(auto(webpackModules, webpackModules.keys()))
+    );
+    const store = createStore(combineReducers(reducers), enhancer);
 
     this.setState({store});
   }
